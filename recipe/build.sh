80559dfc977c6097b15e7d7bad9480b8ce1654d6
Isuru Fernando <isuruf@gmail.com>
2019-08-30 14:28:27 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,9 @@
 
 ./setup.sh
 mkdir -p ${PREFIX}/${PKG_NAME}-${PKG_VERSION}
-cp -r ${SRC_DIR}/* ${PREFIX}/${PKG_NAME}-${PKG_VERSION}
+cp -r ${SRC_DIR}/* ${PREFIX}/${PKG_NAME}-${PKG_VERSION}/
 
-{
-    cd "${PREFIX}/${PKG_NAME}-${PKG_VERSION}"
-    WEST_PYTHON=$(which python2.7)
-    WEST_PYTHON=$WEST_PYTHON $WEST_PYTHON .westpa_gen.py
-    chmod +x westpa.sh
-} >>"${PREFIX}/.messages.txt" 2>&1
+cd "${PREFIX}/${PKG_NAME}-${PKG_VERSION}"
+WEST_PYTHON=$(which python2.7)
+WEST_PYTHON=$WEST_PYTHON $WEST_PYTHON .westpa_gen.py
+chmod +x westpa.sh
