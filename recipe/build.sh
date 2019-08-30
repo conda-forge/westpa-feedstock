ea374f8140478dbfd03b26c09ed9b16d6625dee5
Joshua L. Adelman <joshua.adelman@gmail.com>
2019-08-30 15:59:19 -0400

    debugging in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+set -x
+
 ./setup.sh
 mkdir -p ${PREFIX}/${PKG_NAME}-${PKG_VERSION}
 cp -r ${SRC_DIR}/* ${PREFIX}/${PKG_NAME}-${PKG_VERSION}/
 
 cd "${PREFIX}/${PKG_NAME}-${PKG_VERSION}"
+echo "$(ls -la)"
 WEST_PYTHON=$(which python2.7)
 WEST_PYTHON=$WEST_PYTHON $WEST_PYTHON .westpa_gen.py
 chmod +x westpa.sh
